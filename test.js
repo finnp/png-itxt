aa1f90dcdf8c4baf438ae7b3d5df4a04f5ebac21
Finn <derfinn@gmail.com>
2014-09-18 10:27:36 +0200

    add simple test

---
+++ test.js
@@ -0,0 +1,10 @@
+var fs = require('fs')
+var png = require('./')
+var assert = require('assert')
+
+fs.createReadStream('test.png')
+  .pipe(png.set('cat', 'cute'))
+  .pipe(png.get('cat', function (err, value) {
+    assert.ok(!err)
+    assert.equal(value, 'cute')
+  }))
