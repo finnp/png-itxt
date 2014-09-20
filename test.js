8f39a402aae62d5c58bc0b1dbb06e702a1c2cbe5
Finn <derfinn@gmail.com>
2014-09-20 13:42:05 +0200

    add tape tests

--- test.js
+++ test.js
@@ -1,25 +1,27 @@
 var fs = require('fs')
 var png = require('./')
 var Through = require('stream').PassThrough
+var test = require('tape')
 
 var file = new Buffer('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEX/TQBcNTh/AAAAAXRSTlPM0jRW/QAAAApJREFUeJxjYgAAAAYAAzY3fKgAAAAASUVORK5CYII', 'base64')
 
 var start = new Through()
 
-start
-  .pipe(png.set('cat', 'cute'))
-  .pipe(png.get('cat', function (value) {
-    if(value === 'cute') console.log('pass')
-    else throw new Error('fail')
-  }))
-  .pipe(png.get('pig', function (value) {
-    if(value === null) console.log('pass')
-    else throw new Error('Should be null')
-  }))
-  .pipe(png.set('cat', 'fluffy'))
-  .pipe(png.get('cat', function (value) {
-    if(value === 'fluffy') console.log('pass')
-    else throw new Error('fail')
-  }))
-  
-start.write(file)
+test('png-itxt', function (t) {
+  t.plan(3)
+  start
+    .pipe(png.set('cat', 'cute'))
+    .pipe(png.get('cat', function (value) {
+      t.equal(value, 'cute', 'should get value')
+    }))
+    .pipe(png.get('pig', function (value) {
+      t.deepEqual(value, null, 'should get null')
+    }))
+    .pipe(png.set('cat', 'fluffy'))
+    .pipe(png.get('cat', function (value) {
+      t.equal(value, 'fluffy', 'should only be called once')
+    }))
+    
+  start.write(file)
+})
+
