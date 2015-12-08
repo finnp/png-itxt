cfbec8082ffb447ff98c066338850d37a1b07a10
Brian Cullen <brian.cullen@gmail.com>
2015-12-08 07:09:29 +0000

    Added some more tests to check new functionality.

--- test/multiple-get.test.js
+++ test/multiple-get.test.js
@@ -8,17 +8,24 @@ var file = new Buffer('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEX/T
 var start = new Through()
 
 test('multiple-get', function(t) {
-  t.plan(6)
+  t.plan(10)
   start
+    .pipe(png.get(function(key, value) {
+      t.notOk(key, "no iTXt chunk key to find")
+      t.notOk(value, "no iTXt chunk value to find")
+    }))
     .pipe(png.set('the', 'cat'))
+    .pipe(png.get(function(key, value) {
+      t.same(key, 'the', "single iTXt chunk key returned")
+      t.same(value, 'cat', "single iTXt chunk value returned")
+    }))
     .pipe(png.set('sat', 'on'))
     .pipe(png.set('a', 'mat'))
     .pipe(png.get(function(key, value) {
       t.ok(key, "valid key returned")
       t.ok(value, "valid value returned")
-      console.log(key, value)
-  }))
-  
+    }))
+    
   start.write(file)
 })
 
