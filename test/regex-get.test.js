8d5275caa150c045e7b15ea7d67c9270243fca10
Brian Cullen <brian.cullen@gmail.com>
2015-12-09 10:45:07 +0000

    Tests for using regular expressions as parameters.

---
+++ test/regex-get.test.js
@@ -0,0 +1,34 @@
+var fs = require('fs')
+var png = require('../')
+var Through = require('stream').PassThrough
+var test = require('tape')
+
+var file = new Buffer('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEX/TQBcNTh/AAAAAXRSTlPM0jRW/QAAAApJREFUeJxjYgAAAAYAAzY3fKgAAAAASUVORK5CYII', 'base64')
+
+var start = new Through()
+
+test('regex-get', function(t) {
+  t.plan(10)
+  start
+    .pipe(png.set('the', 'cat'))
+    .pipe(png.set('thhe', 'cat'))
+    .pipe(png.get(/hh/, function(key, value) {
+      t.same(key, 'thhe', "simple regex works for key")
+      t.same(value, 'cat', "simple regex works for value")
+    }))
+    .pipe(png.get(/THE/i, function(key, value) {
+      t.same(key, 'the', "regex with globals works for key")
+      t.same(value, 'cat', "regex with globals works for value")
+    }))
+    .pipe(png.get(/^t[hH]+e/, function(key, value) {
+      t.ok(key, "regex works when option groups defined")
+      t.ok(value, "regex works when option groups defined")
+    }))
+    .pipe(png.get(/THE/, function(key, value) {
+      t.deepEqual(key, null, "regex works when nothing to find")
+      t.deepEqual(value, null, "regex works when nothing to find")
+    }))
+
+  start.write(file)
+})
+
